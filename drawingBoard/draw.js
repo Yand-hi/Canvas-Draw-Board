b4eff2b749fa6ffa4366da57011a2e02160ebc80
Yandhi <15877410869@163.com>
2023-03-31 23:48:56 +0800

    🚀 canvas 画板基础功能

---
+++ drawingBoard/draw.js
@@ -0,0 +1,38 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.querySelector("#canvas")
+canvas.width = document.documentElement.clientWidth
+canvas.height = document.documentElement.clientHeight
+
+const ctx = canvas.getContext("2d")
+
+let painting = false
+let startPoint = {
+  startX: undefined,
+  startY: undefined
+}
+canvas.onmousedown = e => {
+  startPoint.startX = e.x
+  startPoint.startY = e.y
+  painting = true
+}
+canvas.onmousemove = e => {
+  if (painting) {
+    startDraw(startPoint, e.x, e.y)
+    startPoint.startX = e.x
+    startPoint.startY = e.y
+  }
+}
+canvas.onmouseup = () => {
+  painting = false
+}
+
+function startDraw({startX, startY}, x, y) {
+  ctx.beginPath()
+  ctx.moveTo(startX, startY)
+  ctx.lineCap = "round";
+  ctx.lineWidth = 2
+  ctx.lineTo(x, y)
+  ctx.stroke()
+  ctx.closePath()
+}
